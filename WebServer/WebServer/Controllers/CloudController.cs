86b4500e2cb741e7a1c5c0b72b3e04153d28d1eb
pranjkic <49720920+pranjkic@users.noreply.github.com>
2020-11-24 20:15:31 +0100

    initial web server

---
+++ WebServer/WebServer/Controllers/CloudController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServer.Controllers
+{
+    [ApiController]
+    public class CloudController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/message")]
+        public IActionResult GetAllCountries()
+        {
+            return Ok("Hello world.");
+        }
+    }
+}
