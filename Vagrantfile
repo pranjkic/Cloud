5ff9389daad56495628d547ff3662bc9617c466b
pranjkic <49720920+pranjkic@users.noreply.github.com>
2020-12-27 14:06:38 +0100

    starting app by using Vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,10 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "hashicorp/bionic64"
+  config.vm.network "forwarded_port", guest: 8000, host: 8000
+
+  config.vm.provision "file", source: "restApi", destination: "restApi"
+  config.vm.provision "file", source: "restApi/restApi/restApi/.env", destination: ".env"
+  config.vm.provision "file", source: "restApi/restApi/docker-compose.yml", destination: "docker-compose.yml"
   config.vm.provision :shell, path: "script.sh"
 end
--- script.sh
+++ script.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-mkdir html
+#mkdir html
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh get-docker.sh
+
+
+sudo apt-get update
+sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg-agent     software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose up --build -d
